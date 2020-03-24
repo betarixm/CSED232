266e90dadecb886269b9e9645967953a2eb35ebe
beta-lux <beta.underalpha@gmail.com>
2020-03-24 23:35:55 +0900

    Makefile

---
+++ ASSN/ASSN1/assn1_dist.sh
@@ -0,0 +1,11 @@
+rm -rf ./dist
+mkdir ./dist
+mkdir ./dist/20190084
+
+for i in 1 2 3
+do
+  mkdir ./dist/20190084/prob_$i
+  cp ./prob_$i/prob_$i.cpp ./dist/20190084/prob_$i/prob_$i.cpp
+done
+
+cp ./Makefile ./dist/20190084/Makefile
