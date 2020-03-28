13340e9fee70173981d3f814673b75f368947247
beta-lux <beta.underalpha@gmail.com>
2020-03-29 01:03:11 +0900

    comments and report

--- ASSN/ASSN1/prob_1/prob_1.cpp
+++ ASSN/ASSN1/prob_1/prob_1.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 
-int gcd(int w, int h);
+int gcd(int a, int b); // 최대공약수를 구하는 함수
+
+/*
+ * 명예서약 (Honor Code)
+ * 나는 이 프로그래밍 과제를 다른 사람의 부적절한 도움 없이 완수하였습니다.
+ * I completed this programming task without the improper help of others.
+ */
 
 int main(){
-    using namespace std;
+    using namespace std; // std 사용.
 
-    int w = 0, h = 0;
+    int w = 0, h = 0; // w: 모눈 종이의 가로 길이, h: 모눈종이의 세로 길이
 
-    cin >> w >> h;
-    cout << (w * h) - (w + h - gcd(w,h));
+    cin >> w >> h; // 사용자로 부터 모눈종이에 대한 정보를 입력받는다.
+    // 총 모눈종이 칸 수로부터 (가로 + 세로 - 가로와 세로의 최대 공약수)를 빼면 온전한 사각형 갯수를 구할 수 있다.
+    cout << (w * h) - (w + h - gcd(w,h));  // 온전한 사각형의 개수를 출력한다.
 
     return 0;
 }
 
-int gcd(int w, int h){
-    return (h == 0)?(w):(gcd(h, w%h));
+int gcd(int a, int b){ // 최대공약수를 구하는 함수.
+    return (b == 0) ? (a) : (gcd(b, a % b)); // 재귀적으로 유클리드 호제법을 구현하여 최대공약수를 구한다.
 }
--- ASSN/ASSN1/prob_2/prob_2.cpp
+++ ASSN/ASSN1/prob_2/prob_2.cpp
@@ -1,34 +1,53 @@
 #include <iostream>
 #include <vector>
 
+int greedy_max(std::vector<int>& students); // greedy하게 리코더를 가져갈 수 있는 최대의 학생 수를 구하는 함수
+
+/*
+ * 명예서약 (Honor Code)
+ * 나는 이 프로그래밍 과제를 다른 사람의 부적절한 도움 없이 완수하였습니다.
+ * I completed this programming task without the improper help of others.
+ */
+
 int main(){
-    using namespace std;
+    using namespace std; // std 사용
 
-    int student_num = 0, loss_num = 0, extra_num = 0, result_num = 0;
-    int idx = 0;
-    cin >> student_num >> loss_num >> extra_num;
-    vector<int> students(student_num, 0);
+    int student_num = 0, loss_num = 0, extra_num = 0, result_num = 0; // 학생 수 정보들을 담을 변수들 선언
+    int idx = 0; // 인덱스를 위한 임시 변수.
+    cin >> student_num >> loss_num >> extra_num; // 학생 수 정보들을 입력받는다.
+    vector<int> students(student_num, 0); // 전체 학생 수 크기의 벡터를 선언하고 0으로 초기화한다.
 
-    while(loss_num-- && cin >> idx){
-        students[idx - 1] -= 1;
+    while(loss_num-- && cin >> idx){ // 잃어버린 학생 수 만큼 반복
+        students[idx - 1] -= 1; // 학생 벡터에서 잃어버린 학생 번호를 참조하여 1을 뺀다.
     }
 
-    while(extra_num-- && cin >> idx){
-        students[idx - 1] += 1;
+    while(extra_num-- && cin >> idx){ // 여분이 있는 학생 수 만큼 반복
+        students[idx - 1] += 1; // 학생 벡터에서 여분이 있는 학생 번호를 참조하여 1을 더한다.
     }
-    for(idx = 0; idx < students.size(); idx++){
-        if(students[idx] < 0) {
-            if (idx - 1 >= 0 && students[idx - 1] > 0) {
-                students[idx - 1] -= 1;
-                students[idx] += 1;
-            } else if (idx + 1 < students.size() && students[idx + 1] > 0) {
-                students[idx + 1] -= 1;
-                students[idx] += 1;
-            }
-        }
-        result_num += (students[idx] >= 0);
-    }
-    cout << result_num;
+
+    result_num = greedy_max(students); // 리코더를 가져갈 수 있는 최대 학생 수를 구하는 함수 greedy_max 호출.
+
+    cout << result_num; // 최대 값 출력
 
     return 0;
+}
+
+int greedy_max(std::vector<int>& students){ // greedy하게 리코더를 가져갈 수 있는 최대의 학생 수를 구하는 함수
+    using namespace std; // std 사용
+
+    int idx = 0, result_num = 0; // 인덱스 임시 변수와 결과값을 담을 변수 선언.
+    for(idx = 0; idx < students.size(); idx++){ // 학생 벡터 iterate.
+        if(students[idx] < 0) { // 어떤 학생의 값이 0보다 작다면, 즉 리코더를 도난당했다면,
+            if (idx - 1 >= 0 && students[idx - 1] > 0) { // 그런데 앞 번호 친구에게 여분의 리코더가 있다면,
+                students[idx - 1] -= 1; // 여분의 리코더를
+                students[idx] += 1; // 도난 당한 친구에게 대여
+            } else if (idx + 1 < students.size() && students[idx + 1] > 0) { // 앞 번호 친구에게는 없고, 뒷번호 친구에게 있다면,
+                students[idx + 1] -= 1; // 여분의 리코더를
+                students[idx] += 1; // 도난 당한 친구에게 대여
+            }
+        }
+        result_num += (students[idx] >= 0); // 모든 대여 과정이 종료된 후, 학생의 값이 0 이상이라면 리코더를 가져갈 수 있는 학생으로 추가한다.
+    }
+
+    return result_num; // 결과 값 반환.
 }
