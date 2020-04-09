0cd1a6f87f55207bf44f697d671167ede47bdf83
beta-lux <beta.underalpha@gmail.com>
2020-04-10 03:23:32 +0900

    asdf

--- ASSN/ASSN2/List.h
+++ ASSN/ASSN2/List.h
@@ -23,59 +23,50 @@ private:
     int max = 0;
 
 public:
-    explicit List(int size);
-    Node<T>* first = nullptr;
-    Node<T>* last = nullptr;
-    Node<T>* add(T* data);
-    Node<T>* drop(Node<T>* node);
-};
+    explicit List(int max){
+        list = new Node<T>[max];
+        this->max = max;
 
-
-template <typename T>
-List<T>::List(int max){
-    list = new Node<T>[max];
-    this->max = max;
-
-    for(int i=0; i<max; i++){
-        list[i].data = nullptr;
-        list[i].prev = nullptr;
-        list[i].next = nullptr;
-    }
-}
-
-template <typename T>
-Node<T>* List<T>::add(T* data){
-    if (size >= max){
-        return nullptr;
-    }
-    for(int i = 0; i< max; i++){
-        if(list[i].data == nullptr){
-            list[i].data = data;
-            if(size == 0){
-                first = &(list[i]);
-            }
-            list[i].prev = last;
-            last = &(list[i]);
-            size++;
-            return &(list[i]);
+        for(int i=0; i<max; i++){
+            list[i].data = nullptr;
+            list[i].prev = nullptr;
+            list[i].next = nullptr;
         }
     }
-    return nullptr;
-}
 
-template <typename T>
-Node<T>* List<T>::drop(Node<T>* node){
-    if(node->prev == nullptr){
-        first=node->next;
-    } else {
-        (node->prev)->next = node->next;
+    Node<T>* first = nullptr;
+    Node<T>* last = nullptr;
+    Node<T>* add(T* data){
+        if (size >= max){
+            return nullptr;
+        }
+        for(int i = 0; i< max; i++){
+            if(list[i].data == nullptr){
+                list[i].data = data;
+                if(size == 0){
+                    first = &(list[i]);
+                }
+                list[i].prev = last;
+                last = &(list[i]);
+                size++;
+                return &(list[i]);
+            }
+        }
+        return nullptr;
     }
-    delete node->data;
-    node->data = nullptr;
-    node->prev = nullptr;
-    node->next = nullptr;
-    size--;
-    return node;
-}
+    Node<T>* drop(Node<T>* node){
+        if(node->prev == nullptr){
+            first=node->next;
+        } else {
+            (node->prev)->next = node->next;
+        }
+        delete node->data;
+        node->data = nullptr;
+        node->prev = nullptr;
+        node->next = nullptr;
+        size--;
+        return node;
+    }
+};
 
 #endif //CSED232_LIST_H
